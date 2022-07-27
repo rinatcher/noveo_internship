e3709f4f4a949cd42cc3ff7ab89bf1380c714e94
rinatcher <46627353+rinatcher@users.noreply.github.com>
2022-07-27 11:31:22 +0300

    Update task5_result

--- fifth_task/task5_result.rb
+++ fifth_task/task5_result.rb
@@ -1,17 +1,17 @@
-require_relative 'PaginationHelper'
+require_relative 'pagination_helper'
 require_relative 'LinkedList'
 
 helper = PaginationHelper.new(%w[a b c d e f], 4)
-# p helper.page_count # 2
-# p helper.item_count # 6
-# p helper.page_item_count(0) # 4, т.к нумерация страниц с 0
-# p helper.page_item_count(1) # 2, последняя страница содержит 2 элемента
-# p helper.page_item_count(2) # -1
-# # page_index принимает индекс элемента и возвращает страницу, на которой этот элемент находится
-# p helper.page_index(5) # 1
-# p helper.page_index(2) # 0
-# p helper.page_index(-1) # -1
-# p helper.page_index(-10) # -1
+p helper.page_count # 2
+p helper.item_count # 6
+p helper.page_item_count(0) # 4, т.к нумерация страниц с 0
+p helper.page_item_count(1) # 2, последняя страница содержит 2 элемента
+p helper.page_item_count(2) # -1
+# page_index принимает индекс элемента и возвращает страницу, на которой этот элемент находится
+p helper.page_index(5) # 1
+p helper.page_index(2) # 0
+p helper.page_index(-1) # -1
+p helper.page_index(-10) # -1
 
 list = LinkedList.new
 
